ae6f7eb47ef41a7803183f3beb12d50ffe2a38b3
VladyslavBezuhl <alextonov2306@proton.me>
2025-06-11 11:17:11 +0200

    Create application-loadbalancer.tf

---
+++ deploy-to-esc-aws/application-loadbalancer.tf
@@ -0,0 +1,33 @@
+resource "aws_lb" "main" {
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets
+}
+
+resource "aws_lb_target_group" "traefik" {
+  name     = "tg-traefik"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.traefik.arn
+  }
+}
