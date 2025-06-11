23f97d55f75d6cfc32fa129c36882cf70df56543
VladyslavBezuhl <alextonov2306@proton.me>
2025-06-11 11:13:03 +0200

    Create iam.tf

---
+++ deploy-to-esc-aws/iam.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs-instance-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_instance_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ecs_instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_attachment" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
